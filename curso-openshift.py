a61c18a24dc0690ea83f4ac393a2b43de695813e
ridgeBoy <yobegdir@gmail.com>
2020-03-10 17:23:43 +0100

    primer commit
    primer commit

---
+++ curso-openshift.py
@@ -0,0 +1,16 @@
+import os
+from flask import Flask
+app = Flask(__name__)
+
+ 
+
+@app.route("/")
+def main():
+  return "¡¡¡Bienvenido!!!"
+  
+@app.route('/como estas')
+def hola():
+  return 'Estoy bien, gracias'
+  
+if __name__ == "__main__":
+  app.run(host="0.0.0.0", port=8080)
